6c509a7d2303fbfe330ee3873387403d4cd2679f
Richard Jamieson <richard.jamieson@scsuk.net>
2019-06-26 15:01:55 +0000

    Update scs_web_wp_run_container.sh

--- docker/scs_web_wp_run_container.sh
+++ docker/scs_web_wp_run_container.sh
@@ -9,13 +9,4 @@ docker run -d --name=scs_web_wp --network=jamnet --hostname=scs_web_wp --restart
 -e WORDPRESS_DB_PASSWORD_FILE=/secrets/mysql/MYSQL_PASSWORD \
 localhost:5000/docker/wordpress:5.2.2
 
-# The mount of /var/www/html/wp-content screws up its ownership - so need to correct that.
-
-# docker exec scs_web_wp ls -ld /var/www/html/wp-content
-# drwxr-xr-x 4 root root 52 Jun 22 13:54 /var/www/html/wp-content
-
-#docker exec scs_web_wp chown www-data:www-data /var/www/html/wp-content
-
-#docker exec scs_web_wp ls -ld /var/www/html/wp-content
-# drwxr-xr-x 4 www-data www-data 52 Jun 22 13:54 /var/www/html/wp-content
 
